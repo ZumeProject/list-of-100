8a37cfd4bbe97e59de5b953089a5eda2ee8aa200
ChrisChasm <chris@chasm.solutions>
2025-03-24 14:47:49 -0600

    Update app title and branding across multiple platforms, add icon assets, and update web manifest for improved consistency. Include new URL launcher dependency in macOS.

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"zume_list_of_100", origin, size)) {
+  if (!window.Create(L"Zúme List of 100", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
